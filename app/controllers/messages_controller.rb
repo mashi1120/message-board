787592d64fbe20c4d741a4af82b71eb617a35b55
YutaMashine <sk2915.hspp@gmail.com>
2017-01-24 10:44:19 +0000

    challenge2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,4 @@
 class MessagesController < ApplicationController
-  
   before_action :set_message, only: [:edit, :update, :destroy]
   
   def index
@@ -41,7 +40,7 @@ class MessagesController < ApplicationController
 
   private
   def message_params
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :body, :age)
   end
   
   def set_message
